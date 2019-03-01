7be6296c82f41b8a1a0acdd2758e3bd698bad213
eric nana <ericnana@mail.com>
2019-03-02 00:43:12 +0100

    added static folder to myapp folder

--- myapp/settings_/common.py
+++ myapp/settings_/common.py
@@ -14,8 +14,15 @@ import os
 from django.contrib.messages import constants as messages
 #from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
-
-
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 86400  # 1 day
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
